337fee78aa37ae675575318bf1be129012160aeb
durgaprasad192 <durgaprasadravi6@gmail.com>
2026-01-12 01:38:27 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdN1hP05wsatdgO9ybMDq6QIUTO3F85DA",
+  authDomain: "portfolio-4dbe9.firebaseapp.com",
+  projectId: "portfolio-4dbe9",
+  storageBucket: "portfolio-4dbe9.firebasestorage.app",
+  messagingSenderId: "282140333264",
+  appId: "1:282140333264:web:11f53d439f968d2e527ad5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Auth
+export const auth = getAuth(app);
+
+console.log("Firebase connected successfully");
